dbfd6ebea348dfb2af643d80c281b138dc8b5087
vnikolova <viksnikolova@gmail.com>
2021-10-19 16:23:22 +0200

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-
-  return (
-    <div className="App">
-      portfolio
-    </div>
-  );
+  return <div className="App">portfolio</div>;
 }
 
 export default App;
